3deda43d7729a4564b2d86db5fcf385f773a1123
iconsin <soporte@iconsanet.com>
2025-10-16 19:07:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -92,4 +92,5 @@ def webhook():
 # MAIN
 # ==============================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto que Render asigne
+    app.run(host="0.0.0.0", port=port)
